7e726040fcf5e420181f48ee9dc615b17eed97e7
zky <597768024@qq.com>
2020-05-07 20:43:52 +0800

    推荐界面UI完成

---
+++ app/src/main/java/com/example/musicplayer/adapters/RecomendListAdapter.java
@@ -0,0 +1,79 @@
+package com.example.musicplayer.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musicplayer.R;
+import com.squareup.picasso.Picasso;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.AlbumList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecomendListAdapter extends RecyclerView.Adapter<RecomendListAdapter.InnerHolder> {
+    private  List<Album> mData= new ArrayList<>();
+    @NonNull
+    @Override
+    public RecomendListAdapter.InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // 创建每一个item
+        View itemview = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recommend,parent,false);
+        return new InnerHolder(itemview);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecomendListAdapter.InnerHolder holder, int position) {
+        //设置数据
+        holder.itemView.setTag(position);  //每一个位置的item设置一个tag
+        holder.setData(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        // 返回显示的个数
+        if(mData!=null){
+            return mData.size();
+        }
+        return 0;
+    }
+
+    // 每次添加数据都很更新
+    public void setData(List<Album> albumList){
+        if(mData!=null){
+            mData.clear();
+            mData.addAll(albumList);
+        }
+        notifyDataSetChanged();//更新UI
+    }
+
+    // 内部类
+    public class InnerHolder extends RecyclerView.ViewHolder {
+        public InnerHolder(View itemView){
+            super(itemView);
+        }
+
+        public void setData(Album album) {
+            //找到各个控件，设置数据
+            ImageView albumCoverPic=itemView.findViewById(R.id.album_cover); //专辑封面
+            TextView albumTitle =itemView.findViewById(R.id.album_title); //专辑标题
+            TextView albumDesc =itemView.findViewById(R.id.album_description);//专辑描述
+            TextView albumPlayCount =itemView.findViewById(R.id.album_play_count);//播放次数
+            TextView albumContentSize =itemView.findViewById(R.id.album_content_size); //专辑中内容数量
+
+            albumTitle.setText(album.getAlbumTitle()); //调用sdk中的方法
+            albumDesc.setText(album.getAlbumIntro());
+            albumPlayCount.setText(album.getPlayCount()+"");
+            albumContentSize.setText(album.getIncludeTrackCount()+"");
+            Picasso.with(itemView.getContext()).load(album.getCoverUrlLarge()).into(albumCoverPic);
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/musicplayer/fragments/RecommendFragment.java
+++ app/src/main/java/com/example/musicplayer/fragments/RecommendFragment.java
@@ -3,14 +3,74 @@ package com.example.musicplayer.fragments;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.musicplayer.R;
+import com.example.musicplayer.adapters.RecomendListAdapter;
 import com.example.musicplayer.base.BaseFragment;
+import com.example.musicplayer.utils.Constants;
+import com.example.musicplayer.utils.LogUtil;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RecommendFragment extends BaseFragment {
+    private static final String TAG="RecommendFragment";
+    private View mRootView;
+    private RecyclerView mRecommendRv;
+    private RecomendListAdapter mRecommendListAdapter;
     @Override
     protected View onSubViewLoaded(LayoutInflater layoutInflater, ViewGroup container) {
-        View rootView=layoutInflater.inflate(R.layout.fragment_recommend,container,false);
-        return rootView;
+        // View加载完成
+        mRootView=layoutInflater.inflate(R.layout.fragment_recommend,container,false);
+        // RecyclerView的使用
+        mRecommendRv = mRootView.findViewById(R.id.recommend_list); //找到控件
+        LinearLayoutManager linearLayoutManager =new LinearLayoutManager(getContext()); //创建布局管理器
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecommendRv.setLayoutManager(linearLayoutManager); //设置布局管理器
+        mRecommendListAdapter =new RecomendListAdapter(); //创建适配器
+        mRecommendRv.setAdapter(mRecommendListAdapter);
+
+        //获取数据
+        getRecommendData();
+        // 返回view给界面显示
+        return mRootView;
+    }
+
+    //获取推荐内容（SDK文档接口3.10.6）
+    private void getRecommendData(){
+        Map<String, String> map = new HashMap<>();
+        // 一页数据的返回数量
+        map.put(DTransferConstants.LIKE_COUNT, Constants.RECOMMEND_COUNT+"");
+        CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>(){
+            //回调函数,手机记得联网
+            @Override
+            public void onSuccess(@Nullable GussLikeAlbumList gussLikeAlbumList){
+                if(gussLikeAlbumList!=null){
+                    List<Album> albumList=gussLikeAlbumList.getAlbumList();
+                    upRecommendUI(albumList); //获取到数据后更新UI
+                }
+            }
+            @Override
+            public void onError(int i,String s){
+                //报错
+                LogUtil.d(TAG,"ERROR"+i+"-->"+s);
+            }
+        });
+    }
+
+    private void upRecommendUI(List<Album> albumList){
+        //把数据设置给适配器并且更新UI
+        mRecommendListAdapter.setData(albumList);
     }
 }
